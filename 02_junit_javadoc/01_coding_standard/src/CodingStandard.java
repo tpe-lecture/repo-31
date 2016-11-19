881078aae7f7bc5a01b5abe145b2373a530cb7f5
GF Diablo <GF Diablo@GFDiablo-PC.fritz.box>
2016-11-19 20:22:18 +0100

    test

--- 02_junit_javadoc/01_coding_standard/src/CodingStandard.java
+++ 02_junit_javadoc/01_coding_standard/src/CodingStandard.java
@@ -2,27 +2,20 @@
  * Klasse dient als Beispiel für den Coding-Standard und die Namenskonventionen
  * bei Java-Programmen.
  */
-public 
-class 
-CodingStandard {
-
-/*
- * Konstante, die dem Rest der Welt etwas mitteilen soll.
- */
-public final static int konstanteMitTOLLEMWert = 3;
-
-private int ERSTES_Feld;
-private double ZWEITES_Feld;
-
-    /*
-     * Legt eine neue Instanz an.
-     *
-     * Erstes_Feld: das erste Feld.
-     * ZweitesFeld: das zweite Feld.
-     */
-    public CodingStandard(int Erstes_Feld, double ZweitesFeld) 
-    {
-    this.ERSTES_Feld = Erstes_Feld;    this.ZWEITES_Feld = ZweitesFeld;
+    public class CodingStandard {
+        /* Konstante, die dem Rest der Welt etwas mitteilen soll.
+        */
+        public final static int KONSTANTE_MIT_TOLLEM_WERT = 3;
+        private int erstesFeld;
+        private double zweitesFeld;
+        /*
+         * Legt eine neue Instanz an.
+         *
+         * Erstes_Feld: das erste Feld.
+         * ZweitesFeld: das zweite Feld.
+         */
+    public codingStandard(int erstesFeld, double zweitesFeld) {
+    this.erstesFeld = erstesFeld;    this.zweitesFeld = zweitesFeld;
     }
 
     /*
