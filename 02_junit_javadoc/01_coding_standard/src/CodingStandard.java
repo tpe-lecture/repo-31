f976d774b64f58c630ea9c6c017dec1fe397e07a
GF Diablo <GF Diablo@GFDiablo-PC.fritz.box>
2016-11-20 15:44:18 +0100

    Final

--- 02_junit_javadoc/01_coding_standard/src/CodingStandard.java
+++ 02_junit_javadoc/01_coding_standard/src/CodingStandard.java
@@ -1,42 +1,38 @@
-    /**
-    * Klasse dient als Beispiel für den Coding-Standard und die Namenskonventionen
-    * bei Java-Programmen.
+/**
+* Klasse dient als Beispiel für den Coding-Standard und die Namenskonventionen
+*  bei Java-Programmen.
+*/
+public class CodingStandard {
+    /* Konstante, die dem Rest der Welt etwas mitteilen soll.
     */
-    public class CodingStandard {
-        /* Konstante, die dem Rest der Welt etwas mitteilen soll.
-        */
-        public final static int KONSTANTE_MIT_TOLLEM_WERT = 3;
-        private int erstesFeld;
-        private double zweitesFeld;
-        /*
-         * Legt eine neue Instanz an.
-         *
-         * Erstes_Feld: das erste Feld.
-         * ZweitesFeld: das zweite Feld.
-         */
-    public codingStandard(int erstesFeld, double zweitesFeld) {
-    this.erstesFeld = erstesFeld;    this.zweitesFeld = zweitesFeld;
-    }
+  public static final int KONSTANTE_MIT_TOLLEM_WERT = 3;
+  private int erstesFeld;
+  private double zweitesFeld;
+  /*
+  * Legt eine neue Instanz an.
+  *
+  * Erstes_Feld: das erste Feld.
+  * ZweitesFeld: das zweite Feld.
+  */
+  public void codingStandard(int erstesFeld, double zweitesFeld) {
+      this.erstesFeld = erstesFeld;    this.zweitesFeld = zweitesFeld; }
 
-    /*
-     * Methode, die etwas tut.
-     *
-     * parameter: Eingabewert für die Methode.
-     * gibt einen Wert abhängig von {@code parameter} zurück.
-     */
-    public int 
-    Methode_Die_Was_Tut(int Parameter) 
-    {
-    int result;
+  /*
+  * Methode, die etwas tut.
+  *
+  * parameter: Eingabewert für die Methode.
+  * gibt einen Wert abhängig von {@code parameter} zurück.
+  */
+  public int methodeDieWasTut(int parameter){
+      int result;
 
-    if (Parameter > konstanteMitTOLLEMWert)
-    result = 12;        
-    else 
-    {
-        result = 13; }
+      if (parameter > KONSTANTE_MIT_TOLLEM_WERT) {
+		  result = 12;
+	  } else {
+      result = 13; }
 
-    ERSTES_Feld = result; ZWEITES_Feld = 2 * result;
+  erstesFeld = result; zweitesFeld = 2 * result;
 
-    return result;
-    }
+  return result;
+  }
 }
