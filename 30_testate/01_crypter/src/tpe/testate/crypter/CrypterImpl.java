81f1de6781c2b1df21b5ff6bb79d7cfb05184ab0
d.capuana <d.capuana@tom.ki.hs-mannheim.de>
2016-11-21 16:57:49 +0100

    Fertig

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,50 +1,93 @@
 package tpe.testate.crypter;
 
-public class CrypterImpl implements Crypter{
-
-	
+public class CrypterImpl implements Crypter {
+/*
+ * @autor Dario Capuana 1613518
+ * 
+ */
 	@Override
-	public String encrypt(String input) throws IllegalArgumentException{
-		String cryptedText="";
-		String text="";
-		text=input.toLowerCase();
-		char[] array=text.toCharArray();
-		for(int i=0; i<array.length;i++){
-			if((array[i]>47&&array[i]<58)||(array[i]>96&&array[i]<128)||array[i]==32){
-		switch(array[i]){
-		case 'e': { cryptedText=cryptedText+'3'; break;}
-		case '3': { cryptedText=cryptedText+'e'; break;}
-		case 'l': { cryptedText=cryptedText+'1'; break;}
-		case '1': { cryptedText=cryptedText+'l'; break;}
-		case 'o': { cryptedText=cryptedText+'0'; break;}
-		case '0': { cryptedText=cryptedText+'o'; break;}
-		case 'a': { cryptedText=cryptedText+'4'; break;}
-		case '4': { cryptedText=cryptedText+'a'; break;}
-		case 't': { cryptedText=cryptedText+'7'; break;}
-		case '7': { cryptedText=cryptedText+'t'; break;}
-		default: {cryptedText=cryptedText+array[i]; break;
-		} 
-		}
-		
-			}
-			else{
-				throw new IllegalArgumentException();
-			}}
 	
+	/*
+	 * Methode verschluesselt gegebenen String
+	 * @param String input zu verschluesselnder Stringtest
+	 */
+	public String encrypt(String input) throws IllegalArgumentException {
+		String cryptedText = "";
+		String text =input.toLowerCase();
+		char[] array = text.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			if ((array[i] > 47 && array[i] < 58) || (array[i] > 96 && array[i] < 128) || array[i] == 32) {
+				switch (array[i]) {
+				case 'e': {
+					cryptedText = cryptedText + '3';
+					break;
+				}
+				case '3': {
+					cryptedText = cryptedText + 'e';
+					break;
+				}
+				case 'l': {
+					cryptedText = cryptedText + '1';
+					break;
+				}
+				case '1': {
+					cryptedText = cryptedText + 'l';
+					break;
+				}
+				case 'o': {
+					cryptedText = cryptedText + '0';
+					break;
+				}
+				case '0': {
+					cryptedText = cryptedText + 'o';
+					break;
+				}
+				case 'a': {
+					cryptedText = cryptedText + '4';
+					break;
+				}
+				case '4': {
+					cryptedText = cryptedText + 'a';
+					break;
+				}
+				case 't': {
+					cryptedText = cryptedText + '7';
+					break;
+				}
+				case '7': {
+					cryptedText = cryptedText + 't';
+					break;
+				}
+				default: {
+					cryptedText = cryptedText + array[i];
+					break;
+				}
+				}
+
+			} else {
+				throw new IllegalArgumentException();
+				//Exception bei unerlaubten Symbolen
+			}
+		}
+
 		return cryptedText;
 	}
 
 	@Override
+	/*
+	 * Methode entschlüsselt gegebenen String
+	 * @param String input ist der zu entschluesselnde Code
+	 * 
+	 */
 	public String decrypt(String input) throws IllegalArgumentException {
-		for(int i=0; i<input.length();i++){
-			if(Character.isUpperCase(input.charAt(i))) {
+		for (int i = 0; i < input.length(); i++) {
+			//Kontrolle auf Großbuchstaben
+			if (Character.isUpperCase(input.charAt(i))) {
 				throw new IllegalArgumentException();
-		}}
-			
-		return encrypt(input);}
-	
-		
-		
-		
+			}
+		}
+
+		return encrypt(input);
 	}
 
+}
