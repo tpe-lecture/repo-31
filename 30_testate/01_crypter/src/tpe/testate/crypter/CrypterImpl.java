a5b0fd3b3c84771f39d15f79141a17ee615af07a
GFdiablo117 <gfdiablo@web.de>
2016-12-12 20:30:04 +0100

    Add files via upload
    Javadoc, Switch-Case Blöcke, Checkstyle und Exception verbessert

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,94 +1,91 @@
 package tpe.testate.crypter;
 
+/* 
+ * Implementiert Interface crypter und verschluesselt oder entschluesselt strings
+ * @autor Dario Capuana 1613518
+ * 
+ */
+
 public class CrypterImpl implements Crypter {
-    /*
-     * @autor Dario Capuana 1613518
-     * 
-     */
-
-	/*
-	 * Methode verschluesselt gegebenen String
-	 * @param String input zu verschluesselnder Stringtext
-	 */
-	@Override
-	public String encrypt(String input) throws IllegalArgumentException {
-		String cryptedText = "";
-		//grossbuchstaben in kleinbuchstaben umwandeln und als char speichern
-		char[] array = (input.toLowerCase()).toCharArray();
-		for (int i = 0; i < array.length; i++) {
-			//ASCII Code benutzen um unerwuenschte Symbole auszusondern
-			if ((array[i] > 47 && array[i] < 58) || (array[i] > 96 && array[i] < 128) || array[i] == 32) {
-				switch (array[i]) {
-				case 'e': {
-					cryptedText = cryptedText + '3';
-					break;
-				}
-				case '3': {
-					cryptedText = cryptedText + 'e';
-					break;
-				}
-				case 'l': {
-					cryptedText = cryptedText + '1';
-					break;
-				}
-				case '1': {
-					cryptedText = cryptedText + 'l';
-					break;
-				}
-				case 'o': {
-					cryptedText = cryptedText + '0';
-					break;
-				}
-				case '0': {
-					cryptedText = cryptedText + 'o';
-					break;
-				}
-				case 'a': {
-					cryptedText = cryptedText + '4';
-					break;
-				}
-				case '4': {
-					cryptedText = cryptedText + 'a';
-					break;
-				}
-				case 't': {
-					cryptedText = cryptedText + '7';
-					break;
-				}
-				case '7': {
-					cryptedText = cryptedText + 't';
-					break;
-				}
-				default: {
-					cryptedText = cryptedText + array[i];
-					break;
-				}
-				}
-
-			} else {
-				throw new IllegalArgumentException();
-				//Exception bei unerlaubten Symbolen
-			}
-		}
-
-		return cryptedText;
-	}
-
-	@Override
-	/*
-	 * Methode entschluesselt gegebenen String
-	 * @param String input ist der zu entschluesselnde Code
-	 * 
-	 */
-	public String decrypt(String input) throws IllegalArgumentException {
-		for (int i = 0; i < input.length(); i++) {
-			//Kontrolle auf Grossbuchstaben
-			if (Character.isUpperCase(input.charAt(i))) {
-				throw new IllegalArgumentException();
-			}
-		}
-
-		return encrypt(input);
-	}
-
+  /*
+  * Methode verschluesselt gegebenen string
+  * 
+  * @param String input zu verschluesselnder Stringtext
+  * @return String cryptedText verschluesselter Text
+  * 
+  * */
+  @Override
+  public String encrypt(String input)  {
+    String cryptedText = "";
+    //Grossbuchstaben in Kleinbuchstaben umwandeln und als char speichern
+    char[] array = (input.toLowerCase()).toCharArray();
+    for (int i = 0; i < array.length; i++) {
+      //ASCII Code benutzen um unerwuenschte Symbole auszusondern
+      if ((array[i] >= '0' && array[i] <= '9') || (array[i] >= 'a' && array[i] <= 'z') 
+          || array[i] == ' ') {
+        switch (array[i]) {
+          case 'e':
+            cryptedText = cryptedText + '3';
+            break;
+          case '3':
+            cryptedText = cryptedText + 'e';
+            break;
+          case 'l':
+            cryptedText = cryptedText + '1';
+            break;
+          case '1':
+            cryptedText = cryptedText + 'l';
+            break;
+          case 'o':
+            cryptedText = cryptedText + '0';
+            break;
+          case '0':
+            cryptedText = cryptedText + 'o';
+            break;
+          case 'a':
+            cryptedText = cryptedText + '4';
+            break;
+          case '4':
+            cryptedText = cryptedText + 'a';
+            break;
+          case 't':
+            cryptedText = cryptedText + '7';
+            break;
+          case '7':
+            cryptedText = cryptedText + 't';
+            break;
+          default:
+            cryptedText = cryptedText + array[i];
+            break;
+        }
+      }
+    }
+    return cryptedText;
+  }
+  
+  /*
+   * 
+   * Methode die gegebenen String entschluesselt
+   * 
+   * @param String input ist der zu entschluesselnde Text
+   * @return Gibt den entschluesselnten Text zurueck
+   * @throw IllegalArgumentException
+   *          Wird ausgeworfen wenn Eingabe ungültige Zeichen enthaelt
+   *          
+   */
+  @Override
+  public String decrypt(String input) throws IllegalArgumentException {
+    for (int i = 0; i < input.length(); i++) {
+      //Kontrolle auf Grossbuchstaben
+      if (Character.isUpperCase(input.charAt(i))) {
+        throw new IllegalArgumentException();
+      }
+      // Kontrolle auf unerlaubte Zeichen
+      if (!((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') 
+          || input.charAt(i) >= '0' && input.charAt(i) <= '9' || input.charAt(i) == ' ')) {
+        throw new IllegalArgumentException();
+      }
+    }
+    return encrypt(input);
+  }
 }
